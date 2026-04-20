10a7296792caf9f57d3ce577219d7a29008c185e
Dalmar-Campaz <dgcampaz@estudiante.uniajc.edu.co>
2026-04-19 22:33:55 -0500

    Add files via upload
    - se agregaron mas canciones
    - se le agregaron funcionalidades al song.js tales que ahora puede seleccionar canciones para que aparezcan en el player box

--- projects/DaJo - Music Player/js/songs.js
+++ projects/DaJo - Music Player/js/songs.js
@@ -1,13 +1,33 @@
 
 let songs = [];
 const container = document.getElementById("display-songs");
+const urls = [
+    {
+        url: "https://cors-anywhere.herokuapp.com/https://itunes.apple.com/search?term=badbunny&entity=song"
+    },
+    {
+        url: "https://cors-anywhere.herokuapp.com/https://itunes.apple.com/search?term=eminem&entity=song"
+    },
 
+    {
+        url: "https://cors-anywhere.herokuapp.com/https://itunes.apple.com/search?term=the+weeknd&entity=song"
+    },
+    {
+        url: "https://cors-anywhere.herokuapp.com/https://itunes.apple.com/search?term=drake&entity=song"
+    }
+]
 
 async function getSongs() {
     try {
-        const url = "https://cors-anywhere.herokuapp.com/https://itunes.apple.com/search?term=eminem&entity=song";
-        const respuesta = await fetch(url);
-        const songs = await respuesta.json();
+        let songs = [];
+
+        for(const element of urls){
+        const respuesta = await fetch(element.url);
+        const data = await respuesta.json();
+
+        songs.push(...data.results);
+
+        }
 
         return songs;
     } catch (error) {
@@ -17,18 +37,41 @@ async function getSongs() {
 
 async function mostrarSongs() {
     songs = await getSongs();
-    container.innerHTML = "";
-    
-    songs.results.forEach((element) => {
-        let cancion = `
-            <div class="flex-r song" id="${element.trackId}">
+
+    let html = "";
+
+    songs.forEach((element) => {
+        html += `
+            <div class="flex-r song" data-id="${element.trackId}">
               <img src="${element.artworkUrl100}" />
               <p>${element.trackName}</p>
             </div>
         `;
-
-        container.innerHTML += cancion;
     });
+    container.innerHTML = html;
 }
+
+function loadPlayer(song) {
+    const cover = document.getElementById("cover");
+    const title = document.getElementById("title");
+    const artist = document.getElementById("artist");
+
+    cover.src = song.artworkUrl100;
+    title.textContent = song.trackName;
+    artist.textContent = song.artistName;
+}
+
+
+container.addEventListener('click', (e) => {
+    const songDiv = e.target.closest(".song");
+    if (!songDiv) return;
+
+    const id = songDiv.dataset.id;
+    const song = songs.find(s => s.trackId == id);
+    document.querySelectorAll(".song").forEach(s => s.classList.remove("active"));
+    songDiv.classList.add("active");
+
+    loadPlayer(song);
+})
 
 mostrarSongs();
